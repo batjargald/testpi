56c6daca2d1a8a8e98b369110f7113f756c0d4e1
Batjargal <developerjagaa@gmail.com>
2016-08-15 18:25:21 +0900

    just created

---
+++ src/main/java/mn/has/testpi/Application.java
@@ -0,0 +1,11 @@
+package mn.has.testpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main (String[] args){
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ src/main/java/mn/has/testpi/controller/LedController.java
@@ -0,0 +1,31 @@
+package mn.has.testpi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class LedController {
+	
+	public static GpioPinDigitalOutput pin;
+	
+	@RequestMapping("/")
+	public String greeting(){
+		return "Hello world";
+	}
+	
+	@RequestMapping("/li")
+	public String light(){
+		if(pin==null){
+			GpioController gpio = GpioFactory.getInstance();
+			pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "", PinState.LOW);
+		}
+		pin.toggle();
+		return "OK";
+	}
+	
+
+}
