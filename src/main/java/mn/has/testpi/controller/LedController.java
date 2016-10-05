7a4a43f4118544e55cd543106ff15c2e3af8f64f
Batjargal <developerjagaa@gmail.com>
2016-10-05 11:24:12 +0800

    updated

--- src/main/java/mn/has/testpi/controller/LedController.java
+++ src/main/java/mn/has/testpi/controller/LedController.java
@@ -18,11 +18,26 @@ import com.pi4j.io.gpio.RaspiPin;
 public class LedController {
 	
 //	public static GpioPinDigitalOutput pin;
+	final GpioController gpio = GpioFactory.getInstance();
+	 
+	//zuun urd
+	final GpioPinDigitalOutput trasistorLA1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_24, "Transistor LA1", PinState.LOW);
+	final GpioPinDigitalOutput trasistorLA2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_25, "Transistor LA2", PinState.LOW);
+	//zuun hoid
+	final GpioPinDigitalOutput trasistorLB1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_27, "Transistor LB1", PinState.LOW);
+	final GpioPinDigitalOutput trasistorLB2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_28, "Transistor LB2", PinState.LOW);
+	
+	//baruun urd
+	final GpioPinDigitalOutput trasistorRA1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02, "Transistor RA1", PinState.LOW);
+	final GpioPinDigitalOutput trasistorRA2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_03, "Transistor RA2", PinState.LOW);
+	//baruun hoid
+	final GpioPinDigitalOutput trasistorRB1 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "Transistor RB1", PinState.LOW);
+	final GpioPinDigitalOutput trasistorRB2 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_05, "Transistor RB2", PinState.LOW);
 	
 	@RequestMapping("/")
 	public String index(@RequestParam String param1, @RequestParam String mode){
 		if(mode.equals("move")){
-			
+			move(param1);
 		}else{
 			try {
 				int degree = Integer.valueOf(param1);
@@ -34,6 +49,85 @@ public class LedController {
 			}
 		}
 		return "index";
+	}
+	
+	private void move(String mode){
+		switch (mode) {
+		case "w":
+			//zuun uragshaa
+			trasistorLA1.high();
+			trasistorLA2.low();
+			trasistorLB1.high();
+			trasistorLB2.low();
+			
+			//baruun uragshaa
+			trasistorRA1.high();
+			trasistorRA2.low();
+			trasistorRB1.high();
+			trasistorRB2.low();
+			System.out.println("FORWARD ...");
+			break;
+		case "s":
+			//zuun hoishoo
+			trasistorLA1.low();
+			trasistorLA2.high();
+			trasistorLB1.low();
+			trasistorLB2.high();
+			
+			//baruun hoishoo
+			trasistorRA1.low();
+			trasistorRA2.high();
+			trasistorRB1.low();
+			trasistorRB2.high();
+			System.out.println("BACKWARD ...");
+			break;
+		case "d":
+			//zuun uragshaa
+			trasistorLA1.high();
+			trasistorLA2.low();
+			trasistorLB1.high();
+			trasistorLB2.low();
+			
+			//baruun hoishoo
+			trasistorRA1.low();
+			trasistorRA2.high();
+			trasistorRB1.low();
+			trasistorRB2.high();
+			System.out.println("Turn RIGHT");
+			break;
+		case "a":
+			//zuun hoishoo
+			trasistorLA1.low();
+			trasistorLA2.high();
+			trasistorLB1.low();
+			trasistorLB2.high();
+			
+			//baruun uragshaa
+			trasistorRA1.high();
+			trasistorRA2.low();
+			trasistorRB1.high();
+			trasistorRB2.low();
+			System.out.println("Turn LEFT");
+			break;
+		case "q":
+			//zuun zogsoh
+			trasistorLA1.low();
+			trasistorLA2.low();
+			trasistorLB1.low();
+			trasistorLB2.low();
+			
+			//baruun zogsoh
+			trasistorRA1.low();
+			trasistorRA2.low();
+			trasistorRB1.low();
+			trasistorRB2.low();
+			System.out.println("STOP ....");
+			break;
+
+		default:
+			System.out.println("wrong command");
+			break;
+		}
 	}
 	
 //	@RequestMapping("/on")
